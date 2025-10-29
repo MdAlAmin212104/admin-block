90876e7dd842d3aaa8a0353be1b9daca9b3b90ee
Md Al Amin <mdalamin212104@gmail.com>
2025-10-29 16:27:48 +0600

    action extension and blockextension file added for update issue code

--- extensions/create-issue/src/ActionExtension.jsx
+++ extensions/create-issue/src/ActionExtension.jsx
@@ -4,95 +4,141 @@ import { useCallback, useEffect, useState } from "preact/hooks";
 import { getIssues, updateIssues } from "./utils";
 
 export default async () => {
-  {
-    render(<Extension />, document.body);
-  }
+  render(<Extension />, document.body);
+};
 
-  function generateId(allIssues) {
-    return !allIssues?.length ? 0 : allIssues[allIssues.length - 1].id + 1;
-  }
+function generateId(allIssues) {
+  return !allIssues?.length ? 0 : allIssues[allIssues.length - 1].id + 1;
+}
 
-  function validateForm({ title, description }) {
-    return {
-      isValid: Boolean(title) && Boolean(description),
-      errors: {
-        title: !title,
-        description: !description,
-      },
-    };
-  }
+function validateForm({ title, description }) {
+  return {
+    isValid: Boolean(title) && Boolean(description),
+    errors: {
+      title: !title,
+      description: !description,
+    },
+  };
+}
 
-  function Extension() {
-    const { close, data, i18n } = shopify;
-    const [issue, setIssue] = useState({ title: "", description: "" });
-    const [allIssues, setAllIssues] = useState([]);
-    const [formErrors, setFormErrors] = useState(null);
-    const { title, description } = issue;
+function Extension() {
+  const { close, data, intents, i18n } = shopify;
+  const issueId = intents?.launchUrl
+    ? new URL(intents?.launchUrl)?.searchParams?.get("issueId")
+    : null;
+  const [loading, setLoading] = useState(issueId ? true : false);
 
-    useEffect(() => {
-      getIssues(data.selected[0].id).then((issues) =>
-        setAllIssues(issues || []),
-      );
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  const [issue, setIssue] = useState({
+    title: "",
+    description: "",
+    id: issueId,
+  });
+  const [allIssues, setAllIssues] = useState([]);
+  const [formErrors, setFormErrors] = useState(null);
+  const { title, description } = issue;
+  const isEditing = Boolean(issueId);
 
-    console.log(allIssues);
+  useEffect(() => {
+    getIssues(data.selected[0].id).then((issues) => {
+      setLoading(false);
+      setAllIssues(issues || []);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const onSubmit = useCallback(async () => {
-      const { isValid, errors } = validateForm(issue);
-      setFormErrors(errors);
-      console.log("submit button click", allIssues);
-
-      if (isValid) {
-        // Commit changes to the database
-        await updateIssues(data.selected[0].id, [
-          ...allIssues,
-          {
-            id: generateId(allIssues),
-            completed: false,
-            ...issue,
-          },
-        ]);
-        // Close the modal using the 'close' API
-        close();
+  useEffect(() => {
+    if (issueId) {
+      // If opened from the block extension, find the issue that's being edited
+      const editingIssue = allIssues.find(({ id }) => `${id}` === issueId);
+      if (editingIssue) {
+        // Set the issue's ID in the state
+        setIssue(editingIssue);
       }
-    }, [issue, data.selected, allIssues, close]);
+    }
+  }, [issueId, allIssues]);
 
-    return (
-      <s-admin-action heading={i18n.translate("name")}>
-        <s-button slot="primary-action" onClick={onSubmit}>
-          {i18n.translate("issue-create-button")}
-        </s-button>
-        <s-button slot="secondary-actions" onClick={() => {close()}}>
-          {i18n.translate("issue-cancel-button")}
-        </s-button>
-        <s-text-field
-          value={title}
+  const onSubmit = useCallback(async () => {
+    const { isValid, errors } = validateForm(issue);
+    setFormErrors(errors);
+    console.log("submit button click", allIssues);
+
+    if (isValid) {
+      const newIssues = [...allIssues];
+      
+      if (isEditing) {
+        // Find the index of the issue that you're editing
+        const editingIssueIndex = newIssues.findIndex(
+          (listIssue) => listIssue.id == issue.id,
+        );
+        // Overwrite that issue's title and description with the new ones
+        newIssues[editingIssueIndex] = {
+          ...issue,
+          title,
+          description,
+        };
+      } else {
+        // Add a new issue at the end of the list
+        newIssues.push({
+          id: generateId(allIssues),
+          title,
+          description,
+          completed: false,
+        });
+      }
+
+      // Commit changes to the database
+      await updateIssues(data.selected[0].id, newIssues);
+      // Close the modal using the 'close' API
+      close();
+    }
+  }, [issue, data.selected, allIssues, close, isEditing, title, description]);
+
+  if (loading) {
+    return <></>;
+  }
+
+  return (
+    <s-admin-action
+      heading={
+        isEditing
+          ? i18n.translate("edit-issue-heading")
+          : i18n.translate("create-issue-heading")
+      }
+    >
+      <s-button slot="primary-action" onClick={onSubmit}>
+        {isEditing
+          ? "Save"
+          : "Create"}
+      </s-button>
+      <s-button slot="secondary-actions" onClick={close}>
+        Cancel
+      </s-button>
+      <s-text-field
+        value={title}
+        error={
+          formErrors?.title ? i18n.translate("issue-title-error") : undefined
+        }
+        onChange={(event) =>
+          setIssue((prev) => ({ ...prev, title: event.target.value }))
+        }
+        label={i18n.translate("issue-title-label")}
+        maxLength={50}
+      />
+      <s-box padding-block-start="large">
+        <s-text-area
+          value={description}
           error={
-            formErrors?.title ? i18n.translate("issue-title-error") : undefined
+            formErrors?.description
+              ? i18n.translate("issue-description-error")
+              : undefined
           }
           onChange={(event) =>
-            setIssue((prev) => ({ ...prev, title: event.target.value }))
+            setIssue((prev) => ({ ...prev, description: event.target.value }))
           }
-          label={i18n.translate("issue-title-label")}
-          maxLength={50}
+          label={i18n.translate("issue-description-label")}
+          max-length={300}
         />
-        <s-box padding-block-start="large">
-          <s-text-area
-            value={description}
-            error={
-              formErrors?.description
-                ? i18n.translate("issue-description-error")
-                : undefined
-            }
-            onChange={(event) =>
-              setIssue((prev) => ({ ...prev, description: event.target.value }))
-            }
-            label={i18n.translate("issue-description-label")}
-            max-length={300}
-          />
-        </s-box>
-      </s-admin-action>
-    );
-  }
-};
+      </s-box>
+    </s-admin-action>
+  );
+}
--- extensions/issue-tracker-block/src/BlockExtension.jsx
+++ extensions/issue-tracker-block/src/BlockExtension.jsx
@@ -1,42 +1,211 @@
 import { render } from "preact";
-import { useEffect } from "preact/hooks";
-import { getIssues } from "./utils";
+import { useEffect, useMemo, useState } from "preact/hooks";
 
-export default async () => {
-  render(<Extension />, document.body);
-};
+import { updateIssues, getIssues } from "./utils";
 
 const PAGE_SIZE = 3;
 
 function Extension() {
-  const { i18n, data } = shopify;
+  const { data, navigation, i18n } = shopify;
+
+  const [loading, setLoading] = useState(true);
+  const [, setInitialValues] = useState([]);
+  const [issues, setIssues] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const productId = data.selected?.[0]?.id;
+  const issuesCount = issues.length;
+  const totalPages = Math.ceil(issuesCount / PAGE_SIZE);
 
-  console.log({ data, productId, totalPages: PAGE_SIZE });
+  // console.log({ data, productId, totalPages, issuesCount });
 
   useEffect(() => {
     (async function fetchProductIssues() {
       try {
-        console.log("Fetching metafield data for product:", productId);
-        const productData = await getIssues(productId);
+        // console.log("Fetching metafield data for product:", productId);
+        const parsedIssues = await getIssues(productId);
 
-        if (productData) {
-          console.log("✅ Product metafield issues found:", productData);
+        setLoading(false);
+
+        if (parsedIssues && Array.isArray(parsedIssues)) {
+          // console.log("✅ Product metafield issues found:", parsedIssues);
+          setInitialValues(
+            parsedIssues.map(({ completed }) => Boolean(completed)),
+          );
+          setIssues(parsedIssues);
         } else {
           console.log("⚠️ No metafield issues found for this product.");
         }
       } catch (error) {
         console.error("❌ Error fetching product issues:", error);
+        setLoading(false);
       }
     })();
   }, [productId]);
 
+  const paginatedIssues = useMemo(() => {
+    if (issuesCount <= PAGE_SIZE) {
+      return issues;
+    }
+
+    return [...issues].slice(
+      (currentPage - 1) * PAGE_SIZE,
+      currentPage * PAGE_SIZE,
+    );
+  }, [issuesCount, issues, currentPage]);
+
+
+  const handleChange = async (id, value) => {
+    setIssues((currentIssues) => {
+      const newIssues = [...currentIssues];
+      const editingIssueIndex = newIssues.findIndex(
+        (listIssue) => listIssue.id == id,
+      );
+      newIssues[editingIssueIndex] = {
+        ...newIssues[editingIssueIndex],
+        completed: value === "completed" ? true : false,
+      };
+      return newIssues;
+    });
+  };
+
+  const handleDelete = async (id) => {
+    const newIssues = issues.filter((issue) => issue.id !== id);
+    setIssues(newIssues);
+    await updateIssues(productId, newIssues);
+  };
+
+  const onSubmit = (event) => {
+    event.waitUntil(updateIssues(productId, issues));
+  };
+
+  const onReset = () => { };
+
+  if (loading) {
+    return (
+      <s-stack direction="inline">
+        <s-spinner />
+      </s-stack>
+    );
+  }
+
+  // // Show if no issues found
+  // if (!loading && issues.length === 0) {
+  //   return (
+  //     <s-admin-block heading={i18n.translate("name")}>
+  //       <s-stack direction="block">
+  //         <s-text dir="ltr">No issues found for this product.</s-text>
+  //       </s-stack>
+  //     </s-admin-block>
+  //   );
+  // }
+
   return (
     <s-admin-block heading={i18n.translate("name")}>
-      <s-stack direction="block">
-        <s-text type="strong">Welcome</s-text>
-      </s-stack>
+      <s-form id={`issues-form`} onSubmit={onSubmit} onReset={onReset}>
+        {issues.length ? (
+          <>
+            <s-table
+              id="issues-table"
+              paginate={issues.length > 3}  /* ✅ Only paginate if more than 3 */
+              onNextPage={() => setCurrentPage(currentPage + 1)}
+              onPreviousPage={() => setCurrentPage(currentPage - 1)}
+              hasNextPage={currentPage < totalPages}
+              hasPreviousPage={currentPage > 1}
+            >
+              <s-table-header-row>
+                <s-table-header listSlot="primary">
+                  {i18n.translate("issue-column-heading")}
+                </s-table-header>
+                <s-table-header>
+                  {i18n.translate("status-column-heading")}
+                </s-table-header>
+                <s-table-header></s-table-header>
+                <s-table-header></s-table-header>
+              </s-table-header-row>
+              <s-table-body>
+                {paginatedIssues.map(
+                  ({ id, title, description, completed }) => {
+                    return (
+                      <s-table-row key={id}>
+                        <s-table-cell>
+                          <s-stack direction="block">
+                            <s-text type="strong">{title}</s-text>
+                            <s-text>{description}</s-text>
+                          </s-stack>
+                        </s-table-cell>
+                        <s-table-cell>
+                          <s-select
+                            labelAccessibilityVisibility="exclusive"
+                            label={i18n.translate("select-label")}
+                            value={completed ? "completed" : "todo"}
+                            onChange={(value) => handleChange(id, value)}
+                          >
+                            <s-option value="todo">
+                              {i18n.translate("option-todo")}
+                            </s-option>
+                            <s-option value="completed">
+                              {i18n.translate("option-completed")}
+                            </s-option>
+                          </s-select>
+                        </s-table-cell>
+                        <s-table-cell>
+                          <s-button
+                            variant="tertiary"
+                            icon="edit"
+                            accessibilityLabel={i18n.translate(
+                              "edit-issue-button",
+                            )}
+                            onClick={() => {
+                              const url = `extension:create-issue?issueId=${id}`;
+                              navigation?.navigate(url);
+                            }}
+                          />
+                        </s-table-cell>
+                        <s-table-cell>
+                          <s-button
+                            variant="tertiary"
+                            icon="delete"
+                            accessibilityLabel={i18n.translate(
+                              "delete-issue-button",
+                            )}
+                            onClick={() => handleDelete(id)}
+                          />
+                        </s-table-cell>
+                      </s-table-row>
+                    );
+                  },
+                )}
+              </s-table-body>
+            </s-table>
+            <s-button
+              onClick={() => {
+                const url = `extension:create-issue`;
+                navigation?.navigate(url);
+              }}
+            >
+              {i18n.translate("add-issue-button")}
+            </s-button>
+          </>)
+          :
+          (
+            <>
+              <s-button
+                onClick={() => {
+                  const url = `extension:create-issue`;
+                  navigation?.navigate(url);
+                }}
+              >
+                {i18n.translate("add-issue-button")}
+              </s-button>
+            </>
+          )
+        }
+      </s-form>
     </s-admin-block>
   );
 }
+
+export default async () => {
+  render(<Extension />, document.body);
+};
